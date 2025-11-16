2a534837d8163cd24c8737657ea7297ad8314a61
Ed William <edwilliamcardoso@gmail.com.br>
2025-11-16 16:18:24 -0300

    paginacao

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-import { SlideNav } from "./slide.js";
+import SlideNav from "./slide.js";
 
 const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
-
 slide.addArrow(".prev", ".next");
-
-// slide.changeSlide(0);
-// slide.activePrevSlide();
+slide.addControl(".custom-controls");
